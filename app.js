549fbd0e374b2e0d8f1409a08ebaf23fe91b49bb
Violette Crochet <violettecrc@gmail.com>
2025-01-31 16:42:59 +0100

    feat(delete): added post and delete

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ app.get(["/pokemon", "/"], (req, res) => {
         res.status(500).send(err)
     });
 })
+
+app.post("/pokemon", (req,res) => { 
+    let newPokemon = new Pokemon( 
+        req.body
+    );
+    newPokemon.save().then( pokemon => {
+        res.status(201).send(pokemon);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+});
+app.delete("/pokemon/:id", (req, res) => {
+    const id = req.params;
+    Pokemon.findOneAndDelete({id}, (err, pokemon) => {
+        if(err) {
+            res.status(500).send()
+        } else if (!pokemon) {
+            res.status(404).send()
+        } else {
+            res.send("deleted");
+        }
+    })
+});
 // Pokemon.insertMany().then(function () {
 //     console.log("Data inserted") // Success 
 // }).catch(function (error) {
