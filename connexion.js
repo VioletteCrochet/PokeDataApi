2e6c3094448380942ab0ae02533109be94866620
VioletteCrochet <oliviercrochet1@gmail.com>
2025-02-12 20:26:19 +0100

    changed connexion way

--- connexion.js
+++ connexion.js
@@ -1,2 +1,2 @@
-const connexion ="mongodb+srv://Violette:5iaL3IrhTyhBwTlc@cluster0.5p2kq.mongodb.net/PokeData?retryWrites=true&w=majority&appName=Cluster0"
+const connexion ="mongodb+srv://pokeDataUser:5eDj08emcp8IzyUy@cluster0.5p2kq.mongodb.net/PokeData?retryWrites=true&w=majority&appName=Cluster0"
 export {connexion}
